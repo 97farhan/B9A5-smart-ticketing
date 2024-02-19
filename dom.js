49df375a9a3892cf14de599e9bd2d80d7a685f84
97farhan <farhante27@gmail.com>
2024-02-19 09:16:07 -0500

    finally updated

--- dom.js
+++ dom.js
@@ -1,13 +1,17 @@
-const seats = document.querySelectorAll(".kbd");
-let remains = 8;
-for (let index = 0; index < seats.length; index++) {
-    const seat = seats[index];
-    seat.addEventListener("click", function(){
-        this.style.backgroundColor = 'green';
+const cards = document.querySelectorAll(".kbd");
+for (let index = 0; index < cards.length; index++) {
+    const card = cards[index];
+    card.addEventListener("click", function(){
+        this.style.backgroundColor = 'green'
+        const seat = card.querySelector("h1").innerText;
+        const dataContainer = document.getElementById("row-one");
+        const data = document.createElement("p")
+        
+        data.innerText = seat;
+        dataContainer.appendChild(data);
+
         
     })
-    
 }
-
 
 
